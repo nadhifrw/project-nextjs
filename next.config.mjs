0547b10ea3ab8c397c90e58d1b707c529e2d8bbf
nadhifrw <hernadhif.rawir@gmail.com>
2024-10-22 21:05:05 +0700

    testing deploying using github

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+Copy/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',  // Required for static site generation
+  basePath: '/project-nextjs', // Should match your repository name
+  images: {
+    unoptimized: true // Required for static export
+  }
+}
+
+module.exports = nextConfig
